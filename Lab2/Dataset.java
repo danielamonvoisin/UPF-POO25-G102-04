794577b2c8d2cc6123220bbe3fa7d542f4ceab1d
Anna Vargas <anna.vargas01@estudiant.upf.edu>
2025-11-03 17:17:12 +0100

    everything before creating the StandarizedDataset

--- Lab2/Dataset.java
+++ Lab2/Dataset.java
@@ -1,10 +1,11 @@
 package Lab2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Dataset{
-    private ArrayList <Record> data;
+    private ArrayList <Record> data; //aggregation relationship with the Record class
     private final int dim;
 
     public Dataset(int D){
@@ -25,5 +26,75 @@ public class Dataset{
 
     public void addRecord(Record record){
         this.data.add(record);
+    }
+
+    public Vector meanInput() {
+    if (data.isEmpty()) {
+        return new Vector(dim, 0.0);
+    }
+
+    Vector sum = new Vector(dim, 0.0);
+    for (Record record : data) {
+        Vector inputVector = new Vector(dim, record.getX());
+        sum = sum.add(inputVector);
+    }
+    return sum.DivideScalar(data.size());
+    }
+
+    public Vector stdInput() {
+        if (data.isEmpty()) {
+            return new Vector(dim, 0.0);
+        }
+
+        Vector mean = meanInput();
+        Vector sumSquaredDifferences = new Vector(dim, 0.0);
+
+        for (Record record : data) {
+            Vector inputVector = new Vector(dim, record.getX());
+            Vector difference = inputVector.subtract(mean);
+            Vector squaredDifference = difference.MultiplyNumbers(difference);
+            sumSquaredDifferences = sumSquaredDifferences.add(squaredDifference);
+        }
+        
+        return sumSquaredDifferences.DivideScalar(data.size()).SquareRoot();
+    }
+
+    public double meanOutput() {
+        if (data.isEmpty()) {
+            return 0.0;
+        }
+
+        double sum = 0.0;
+        for (Record record : data) {
+            sum += record.getY();
+        }
+        return sum / data.size();
+    }
+
+    public double stdOutput() {
+        if (data.isEmpty()) {
+            return 0.0;
+        }
+
+        double mean = meanOutput();
+        double sumSquaredDifferences = 0.0;
+
+        for (Record record : data) {
+            double difference = record.getY() - mean;
+            sumSquaredDifferences += difference * difference;
+        }
+
+        double variance = sumSquaredDifferences / data.size();
+        return Math.sqrt(variance);
+    }
+
+    //WE ARE MISSING STANDARIZE()
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Record record : data) {
+            sb.append("  ").append(record.convertString()).append("\n");
+        }
+        return sb.toString();
     }
 }
